85b40fdf02f5c19b5dbf1d4bf75055ff70f19c3d
browerjosiahdev <josiahb@allencomm.com>
2015-11-04 09:18:59 -0700

    updated methods and varibles with the svgi prefix so it will remain unique when loaded into other libraries. Also removed the auto run call, so the svgi_run method must be called manually.

--- public/js/svg_injector.js
+++ public/js/svg_injector.js
@@ -1,5 +1,13 @@
-var console;
-if (console === undefined) {
+/*
+ * Copyright (c) 2015 Josiah James Brower
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.var console;
+*/
+if (typeof console === 'undefined') {
 	console = {
 		"log": function(v){},
 		"dir": function(v){},
@@ -7,20 +15,20 @@ if (console === undefined) {
 	}
 }
 
-var svgData = Object.create(null);
+var svgi_svgData = Object.create(null);
 
-function init() {
+function svgi_run() {
 	var elements = document.querySelectorAll('[data-svg-src]');
 	
 	for (var inElements = 0; inElements < elements.length; inElements++) {
 		var element = elements[inElements],
 		    svgSrc  = element.getAttribute('data-svg-src');
 			
-		var id = generateId();
-		svgData[id] = element;
+		var id = svgi_generateId();
+		svgi_svgData[id] = element;
 		
-		loadFile(svgSrc, function( xhr, data ){
-			var svgElement = svgData[data.id],
+		svgi_loadFile(svgSrc, function( xhr, data ){
+			var svgElement = svgi_svgData[data.id],
 			    response   = xhr.responseText;
 			
 			svgElement.innerHTML = svgElement.innerHTML + response.substr(response.indexOf('<svg'), response.length);
@@ -28,7 +36,7 @@ function init() {
 	}
 }
 
-function loadFile( src, callback, data /*optional*/ ) {
+function svgi_loadFile( src, callback, data /*optional*/ ) {
 	var xhr;
 	
 	if (typeof XMLHttpRequest !== 'undefined') {
@@ -52,7 +60,7 @@ function loadFile( src, callback, data /*optional*/ ) {
 					callback(xhr);
 				}
 			} else {
-				console.warn("loadFile('" + src + "') request failed: " + xhr.readyState);
+				console.warn("svgi_loadFile('" + src + "') request failed: " + xhr.readyState);
 			}
 		}
 		
@@ -61,10 +69,8 @@ function loadFile( src, callback, data /*optional*/ ) {
 	}
 }
 
-function generateId() {
+function svgi_generateId() {
 	return '0000000000'.replace(/\d/g, function( char ) {
 		return String.fromCharCode(97 + Math.floor(Math.random() * 26));
 	});
 }
-
-init();
